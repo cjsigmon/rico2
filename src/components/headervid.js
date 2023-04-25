cf57e6a4e8c7a21d6b4cb56e395f8b1f4847a747
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-04-25 17:47:31 -0400

    new build

--- src/components/headervid.js
+++ src/components/headervid.js
@@ -1,23 +1,12 @@
 import * as React from "react";
 import '../styles.css';
 import VideoComp from "./videoComp";
-import ReactPlayer from "react-player";
 
 export default function HeaderVid() {
 
     return (
         <div className={"bg bg-home"} >
-            <ReactPlayer
-                  
-                 
-                  style={{ position: 'absolute', top:'0', left:'0', width: '100%', height:'100%'}}
-                  url={"https://player.vimeo.com/video/821024502?h=cdcad75069&autoplay=1&title=0&byline=0&portrait=0&controls=0"}
-                  
-                  
-                  
-                  
-              />
-            
+            <div style={{padding:"56.25% 0 0 0", position:"relative"}}><iframe src="https://player.vimeo.com/video/821024502?h=cdcad75069&autoplay=1&title=0&byline=0&portrait=0&controls=0" style={{position:"absolute", top:"0", left:"0", width:"100%", height:"100%"}} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen muted playsinline poster="img/"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
           </div>
     );
 }
