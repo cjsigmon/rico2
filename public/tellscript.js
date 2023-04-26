0c50c766e9cdea64c0a359764788f4d2c47b0a9e
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-04-25 21:53:08 -0400

    updated

---
+++ public/tellscript.js
@@ -0,0 +1,52 @@
+var i = 1;
+const linkHead = 'https://flo.uri.sh/story/1894096/embed#slide-';
+const isBrowser = typeof window !== "undefined";
+
+
+function handleScroll() {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+    const windowHeight = document.documentElement.clientHeight || window.innerHeight;
+    const scrollPercentage = (scrollTop / (scrollHeight - windowHeight)) * 100;
+
+    if (scrollPercentage > 80) {
+        console.log('Scroll position is above 80%');
+        i = 4;
+    } else if (scrollPercentage > 60) {
+        console.log('Scroll position is above 60%');
+        i = 3;
+    }
+    else if (scrollPercentage > 40) {
+        console.log('Scroll position is above 40%');
+        i = 2;
+    }  else if (scrollPercentage > 20) {
+        console.log('Scroll position is above 20%');
+        i = 1;
+    }
+    else {
+        i = 0;
+    }
+
+            var miframe = document.getElementById("miframe");
+            if (miframe) {
+                miframe.src = (linkHead + "" + i);
+            }        
+      }
+
+    
+      let scrollTimeout;
+      if (isBrowser) {
+        window.addEventListener('scroll', () => {
+            // clear any existing timeout
+            clearTimeout(scrollTimeout);
+            var miframe = document.getElementById("miframe");
+            
+            // set a new timeout to delay the handleScroll() function
+            scrollTimeout = setTimeout(() => {
+          
+              handleScroll();
+            }, 100); // 500 milliseconds delay
+          });
+      }
+
+
--- src/components/headervid.js
+++ src/components/headervid.js
@@ -1,12 +1,21 @@
 import * as React from "react";
 import '../styles.css';
 import VideoComp from "./videoComp";
+import logo from '../img/logo/LOGO-color.png';
 
 export default function HeaderVid() {
 
     return (
         <div className={"bg bg-home"} >
-            <div style={{padding:"56.25% 0 0 0", position:"relative"}}><iframe src="https://player.vimeo.com/video/821024502?h=cdcad75069&autoplay=1&title=0&byline=0&portrait=0&controls=0" style={{position:"absolute", top:"0", left:"0", width:"100%", height:"100%"}} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen muted playsinline poster="img/"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+            
+            {/* <div style={{padding:"56.25% 0 0 0", position:"relative"}}><iframe src="https://player.vimeo.com/video/821024502?h=cdcad75069&autoplay=1&title=0&byline=0&portrait=0&controls=0" style={{position:"absolute", top:"0", left:"0", width:"100%", height:"100%"}} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen muted playsinline poster="img/"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script> */}
+            <div className="box">
+                <div id="main-title-contain">
+                  <img id="home-title" src={logo} width={"70%"}/>
+                  
+                </div> 
+            </div>
+          
           </div>
     );
 }
